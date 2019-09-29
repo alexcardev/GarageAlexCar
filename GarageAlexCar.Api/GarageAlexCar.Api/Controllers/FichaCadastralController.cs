f3b2ed0dbb65aa59c80d94d4d963f0c93a1beafd
Alexandre Carvalho <alexandre.carvalho@live.com>
2019-09-29 11:24:48 -0300

    Revert "Criação da pesquisa por identificação do automóvel."
    This reverts commit 652bb36531422ed9691c82b67e880a4a6419631c.

--- GarageAlexCar.Api/GarageAlexCar.Api/Controllers/FichaCadastralController.cs
+++ GarageAlexCar.Api/GarageAlexCar.Api/Controllers/FichaCadastralController.cs
@@ -17,12 +17,5 @@ namespace GarageAlexCar.Api.Controllers
         {
             return new string[] { "value1", "value2" };
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
     }
 }
